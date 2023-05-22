d8057f5530e826b9387da985381d57dfd4e73840
Matias <matiasdv1@hotmail.com>
2023-05-21 21:10:31 -0300

    django-hello-world

--- fileWizardAPI/urls.py
+++ fileWizardAPI/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/hello_world', views.hello_world, name='test' ),
 ]
